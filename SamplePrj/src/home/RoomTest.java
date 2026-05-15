5b123cda1eddad92a82459aacd65ae88f2dad8a2
재용 성 <mavi1969@naver.com>
2026-05-15 20:53:21 +0900

    v1

--- SamplePrj/src/home/RoomTest.java
+++ SamplePrj/src/home/RoomTest.java
@@ -1,10 +1,17 @@
 package home;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RoomTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Set<String> keySets = new HashSet<>(Arrays.asList("a", "b"));
 
+		System.out.println("test");
 	}
 
 }
